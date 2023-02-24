59ae8281bbe19061305de5c6a8c3219ae0c9baac
vivek001100 <vivek.mittal@byjus.com>
2023-02-24 13:25:17 +0530

    2 commit

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-const express=require('express');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const authEmailModel = require("./models/authEmailmodel");
 
-const app=express()
-const port =7000;
-app.listen(port,()=>{
-    console.log(`connection succesfull at port : ${port}`);
-})
+const app = express();
+
+const routes = require("./routes");
+
+app.use(cors({ origin: "*" }));
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/", routes);
+
+const port = 7000;
+// setInterval(() => {
+//   authEmailModel.update_login_trial_for_all(0);
+// }, 60000);
+
+
+
+app.listen(port, () => console.log(`server listening at port ${port}....`));
