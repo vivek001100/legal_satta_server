cae66ffcec2ae32971089a956eb03e59fe604b2e
saqibk999 <saqib.mohd1@byjus.com>
2023-02-28 15:34:28 +0530

    committing perfect

--- controllers/matchController.js
+++ controllers/matchController.js
@@ -3,14 +3,26 @@ const teamModel=require('./../models/teamModel')
 
 exports.getLatestMatch=async(req,res,next)=>{
 const date=new Date()
-const queryField={
-    date:`${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`
+
+const day=date.getMonth()+1
+let queryField;
+if(day<10){
+     queryField={
+        date:`${date.getDate()}-0${date.getMonth()+1}-${date.getFullYear()}`
+    }
 }
+else{
+     queryField={
+        date:`${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`
+    }
+}
+
 
 const teamIdList=[];
 
 console.log(queryField)
 const todayMatch=(await matchModel.getMatch(queryField))[0]
+console.log(todayMatch)
 const team1Id=todayMatch.team1
 const team2Id=todayMatch.team2
 
@@ -31,4 +43,8 @@ res.status(200).json({
     status:"success",
     result:todayMatch
 })
+}
+
+exports.getUpcomingMatch=async(req,res,next)=>{
+    
 }
--- routes/index.js
+++ routes/index.js
@@ -2,24 +2,13 @@ const express = require("express");
 const app = express();
 
 const authRouter = require("./authRoutes");
-// const userRoutes=require('./userRoutes')
 const matchRoutes = require("./matchRoutes");
-
 const userChoiceRoutes = require("./usersChoiceRoutes");
-
 const leaderboardRoutes = require("./leaderboardRoutes");
 
-app.use("/auth", authRouter);
-// app.use('/user',userRoutes)
 app.use("/match", matchRoutes);
 app.use("/get", leaderboardRoutes);
 app.use("/postchoice", userChoiceRoutes);
-const authRouter=require('./authRoutes')
-// const userRoutes=require('./userRoutes')
-const matchRoutes=require('./matchRoutes')
+app.use("/auth", authRouter);
 
-app.use('/auth',authRouter)
-// app.use('/user',userRoutes)
-app.use('/match',matchRoutes)
-
-module.exports=app
+module.exports = app;
