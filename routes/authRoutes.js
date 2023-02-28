02d1a887a1c7979321f424e6ea0f028ffa00d247
vivek001100 <vivek.mittal@byjus.com>
2023-02-28 11:52:38 +0530

    get latest match

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,5 +3,4 @@ const router = express.Router();
 const authController = require("./../controllers/authControllers");
 
 router.post("/signup",authController.signup);
-router.post('registation',authController.registration);
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ const express=require('express')
 
 const app=express()
 const authRouter=require('./authRoutes')
+const userRoutes=require('./userRoutes')
+const matchRoutes=require('./matchRoutes')
 
 app.use('/auth',authRouter)
+app.use('/user',userRoutes)
+app.use('/match',matchRoutes)
+
 module.exports=app
