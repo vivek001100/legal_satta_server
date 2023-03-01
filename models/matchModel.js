7b4476afa3db6f2e04451925d25feb6010944cdf
vivek001100 <vivek.mittal@byjus.com>
2023-03-01 18:12:45 +0530

    auth change

--- models/matchModel.js
+++ models/matchModel.js
@@ -8,13 +8,13 @@ exports.getUpcomingMatches = (queryfield) => {
   return db("match").select("*").where("date", ">", queryfield);
 };
 
-exports.getLastPlayedMatch = () => {
-  return db("match")
-    .select("*")
-    .whereNotNull("winner")
-    .orderBy("date", "desc")
-    .limit(1);
-};
+// exports.getLastPlayedMatch = () => {
+//   return db("match")
+//     .select("*")
+//     .whereNotNull("winner")
+//     .orderBy("date", "desc")
+//     .limit(1);
+// };
 
 exports.updateWinner = (date, winner) => {
   return db("match").where({ date }).update({ winner });
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,7 @@ const authController = require("./../controllers/authControllers");
 
 router.post("/signup",authController.signup);
 router.post("/login",authController.login);
+router.post("/verify",authController.verifyToken);
 
 
 
