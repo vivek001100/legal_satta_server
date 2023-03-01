456ec18f39aa23fef204e337a38d52765c5d9c5b
vivek001100 <vivek.mittal@byjus.com>
2023-03-01 11:17:16 +0530

    leaderboard complete

--- controllers/leaderboardController.js
+++ controllers/leaderboardController.js
@@ -1,11 +1,29 @@
 const leaderboardModel = require("./../models/leaderboardModel");
 const matchModel = require("./../models/matchModel");
+
 exports.getLeaderboards = async (req, res, next) => {
   const leaderboard = await leaderboardModel.getLeaderboard();
-  const lastMatch = await matchModel.getLastPlayedMatch();
+  const lastMatchQuery = await matchModel.getLastPlayedMatch();
+  const lastMatch = lastMatchQuery[0];
+
+  const team1Id = lastMatch.team1;
+  const team2Id = lastMatch.team2;
+
+  teamIdList.push(team1Id);
+  teamIdList.push(team2Id);
+
+  const teams = await teamModel.getTeams(teamIdList);
+
+  lastMatch.team1 = teams.filter((e) => e.id === team1Id)[0];
+  lastMatch.team2 = teams.filter((e) => e.id === team2Id)[0];
+
   console.log(lastMatch);
+
   res.status(200).json({
     status: "success",
-    result: leaderboard,
+    result: {
+      lastMatch,
+      leaderboard,
+    },
   });
 };
